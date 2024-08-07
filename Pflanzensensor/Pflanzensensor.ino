e1bb9a646823dde605d57d274fbfa476cf86f21c
Tommy Schönherr <tommy.schoenherr@posteo.de>
2024-08-07 17:42:08 +0200

    Serielle Ausgabe für den Timestamp nun Deutsch

--- Pflanzensensor/Pflanzensensor.ino
+++ Pflanzensensor/Pflanzensensor.ino
@@ -137,7 +137,7 @@ void setup() {
   #if MODUL_WEBHOOK
     // Set time via NTP, as required for x.509 validation
     configTime(3 * 3600, 0, "pool.ntp.org", "time.nist.gov");
-    Serial.print("Waiting for NTP time sync: ");
+    Serial.print("Warte auf die Synchronisation von Uhrzeit und Datum: ");
     time_t now = time(nullptr);
     while (now < 8 * 3600 * 2) {
       delay(500);
@@ -147,7 +147,7 @@ void setup() {
     Serial.println("");
     struct tm timeinfo;
     gmtime_r(&now, &timeinfo);
-    Serial.print("Current time: ");
+    Serial.print("Die Zeit und das Datum ist: ");
     Serial.print(asctime(&timeinfo));
   #endif
   if (VariablenDa()) {
